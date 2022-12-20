bec132ddf0d394a0f42a12bb4f64bf27e7b9c766
Martin Peeters <martin.peeters@affinitic.be>
2022-12-20 22:58:22 +0100

    Black & cleanup

--- ZemAppDelegate.py
+++ ZemAppDelegate.py
@@ -7,25 +7,22 @@
 #
 
 # library imports
-import os, re
 from objc import YES, NO
 from PyObjCTools import AppHelper
-
-# import needed classes/functions from Foundation
-from Foundation import *
-
-# import Nib loading functionality from AppKit
-from AppKit import *
-
-# local product imports
+from Foundation import *  # noqa
+from AppKit import *  # noqa
 from ZopeDocument import ZopeDocument, __version__
 from PreferenceController import PreferenceController
+
+import os
+
 
 def iterNSIndexSet(s):
     val = s.lastIndex()
     while val != NSNotFound:
         yield val
         val = s.indexLessThanIndex_(val)
+
 
 class ZemAppDelegate(NSObject):
     """
@@ -49,7 +46,12 @@ class ZemAppDelegate(NSObject):
                 self.sync_spinner.startAnimation_(self)
                 doc.putChanges()
                 self.sync_spinner.stopAnimation_(self)
-                self.sync_message.setStringValue_(u'Last synched: %s' % NSDate.date().descriptionWithCalendarFormat_timeZone_locale_(u"%a, %d %b %Y at %H:%M:%S %p", None, None))
+                self.sync_message.setStringValue_(
+                    "Last synched: %s"
+                    % NSDate.date().descriptionWithCalendarFormat_timeZone_locale_(
+                        "%a, %d %b %Y at %H:%M:%S %p", None, None
+                    )
+                )
                 doc.last_mtime = mtime
 
         return 1
@@ -71,7 +73,7 @@ class ZemAppDelegate(NSObject):
         """
         Invoked by NSApplication when the app attempts to open a file
         """
-        NSLog(u'Opening Application')
+        NSLog("Opening Application")
 
         zopeDoc = ZopeDocument(filename)
         zopeDoc.removeFileIfNecessary(filename)
@@ -88,27 +90,35 @@ class ZemAppDelegate(NSObject):
 
     @objc.IBAction
     def openDocument_(self, zopeDoc):
-        if self.sud.boolForKey_(u'use_locks'):
-            if zopeDoc.metadata.get(u'lock-token'):
-                if (self.sud.boolForKey_(u'always_borrow_locks')
-                    or zopeDoc.metadata.get(u'borrow_lock')
-                    or NSRunAlertPanel(u"Borrow Lock?",
-                                       u"This object is already locked by"
-                                       " you in another session. Do you want"
-                                       " to borrow this lock and continue?",
-                                       u"Yes", u"Cancel", None)):
-                    zopeDoc.lock_token = (u'opaquelocktoken:%s'
-                                          % zopeDoc.metadata['lock-token'])
+        if self.sud.boolForKey_("use_locks"):
+            if zopeDoc.metadata.get("lock-token"):
+                if (
+                    self.sud.boolForKey_("always_borrow_locks")
+                    or zopeDoc.metadata.get("borrow_lock")
+                    or NSRunAlertPanel(
+                        "Borrow Lock?",
+                        "This object is already locked by"
+                        " you in another session. Do you want"
+                        " to borrow this lock and continue?",
+                        "Yes",
+                        "Cancel",
+                        None,
+                    )
+                ):
+                    zopeDoc.lock_token = (
+                        "opaquelocktoken:%s" % zopeDoc.metadata["lock-token"]
+                    )
             else:
                 self.finishEdits_()
 
-        if self.sud.boolForKey_(u'use_locks'):
+        if self.sud.boolForKey_("use_locks"):
             zopeDoc.lock()
 
         editor = zopeDoc.getEditor()
         content_file = zopeDoc.getContentFile()
-        return self.ws.openFile_withApplication_andDeactivate_(content_file,
-                                                               editor, YES)
+        return self.ws.openFile_withApplication_andDeactivate_(
+            content_file, editor, YES
+        )
 
     @objc.IBAction
     def finishEdits_(self, sender=None):
@@ -119,14 +129,14 @@ class ZemAppDelegate(NSObject):
         selected = self.current_edits.numberOfSelectedRows()
         if selected:
             perform = True
-            if self.sud.boolForKey_(u'confirm_on_finish'):
+            if self.sud.boolForKey_("confirm_on_finish"):
                 if selected == 1:
-                    msg = u"Are you sure you're finished with this object?"
+                    msg = "Are you sure you're finished with this object?"
                 else:
-                    msg = (u"Are you sure you're finished with these %d "
-                           "objects?" % (selected))
-                perform = NSRunAlertPanel(u"Deleting Entry", msg,
-                                          u"Yes", u"Cancel", None)
+                    msg = "Are you sure you're finished with these %d " "objects?" % (
+                        selected
+                    )
+                perform = NSRunAlertPanel("Deleting Entry", msg, "Yes", "Cancel", None)
             for row in iterNSIndexSet(self.current_edits.selectedRowIndexes()):
                 del self.current_edits_data[row]
                 self.current_edits.reloadData()
@@ -134,10 +144,9 @@ class ZemAppDelegate(NSObject):
     def numberOfRowsInTableView_(self, tableView):
         return len(self.current_edits_data)
 
-    def tableView_objectValueForTableColumn_row_(self, tableView,
-                                                 aColumn, aRow):
+    def tableView_objectValueForTableColumn_row_(self, tableView, aColumn, aRow):
         ident = aColumn.identifier()
-        return getattr(self.current_edits_data[aRow], 'get' + ident)()
+        return getattr(self.current_edits_data[aRow], "get" + ident)()
 
     def tableViewSelectionDidChange_(self, aNotification):
         if self.current_edits.selectedRow() == -1:
@@ -147,55 +156,72 @@ class ZemAppDelegate(NSObject):
 
     def upgradePrefs(self):
         new_prefs = {}
-        keep_keys = [u'always_borrow_locks', u'cleanup_files',
-                     u'confirm_on_finish', u'helper_apps', u'save_interval',
-                     u'use_locks', u'version_check']
+        keep_keys = [
+            "always_borrow_locks",
+            "cleanup_files",
+            "confirm_on_finish",
+            "helper_apps",
+            "save_interval",
+            "use_locks",
+            "version_check",
+        ]
         for key in keep_keys:
             new_prefs[key] = self.sud.objectForKey_(key)
         edit_keys = self.sud.persistentDomainForName_(self.bundleIdent).keys()
-        helper_apps = self.sud.dictionaryForKey_(u'helper_apps')
+        helper_apps = self.sud.dictionaryForKey_("helper_apps")
         if helper_apps is None:
             helper_apps = {}
         for key in edit_keys:
             if key not in keep_keys:
-                if self.sud.objectForKey_(key).has_key(u'editor'):
+                if self.sud.objectForKey_(key).has_key("editor"):
                     helper_apps[key] = self.sud.objectForKey_(key)
                 else:
                     sub_keys = self.sud.objectForKey_(key).keys()
                     for sub_key in sub_keys:
-                        helper_apps[u"%s/%s" % (key, sub_key)] = self.sud.objectForKey_(key)[sub_key]
-        new_prefs[u'helper_apps'] = helper_apps
+                        helper_apps["%s/%s" % (key, sub_key)] = self.sud.objectForKey_(
+                            key
+                        )[sub_key]
+        new_prefs["helper_apps"] = helper_apps
 
         # Possibly a new pref this version
-        if not new_prefs.has_key(u'confirm_on_finish'):
-            new_prefs[u'confirm_on_finish'] = YES
+        if not new_prefs.has_key("confirm_on_finish"):
+            new_prefs["confirm_on_finish"] = YES
 
         # Update this pref always
-        new_prefs[u'version_check'] = __version__
+        new_prefs["version_check"] = __version__
 
         self.sud.removePersistentDomainForName_(self.bundleIdent)
         self.sud.setPersistentDomain_forName_(new_prefs, self.bundleIdent)
         self.sud.synchronize()
-        NSRunAlertPanel(u"Preferences Updated",
-                        u"Your Preferences have been upgraded.",
-                        u"Close", None, None)
+        NSRunAlertPanel(
+            "Preferences Updated",
+            "Your Preferences have been upgraded.",
+            "Close",
+            None,
+            None,
+        )
 
     def resetPrefs(self):
         self.sud.removePersistentDomainForName_(self.bundleIdent)
         self.sud.setPersistentDomain_forName_(
             NSDictionary.dictionaryWithContentsOfFile_(
-                NSBundle.mainBundle().pathForResource_ofType_(
-                    self.bundleIdent, "plist")),
-            self.bundleIdent)
+                NSBundle.mainBundle().pathForResource_ofType_(self.bundleIdent, "plist")
+            ),
+            self.bundleIdent,
+        )
         self.sud.synchronize()
-        if NSRunAlertPanel(u"Preferences Reset",
-                           u"Your Preferences have been reset.",
-                           u"Edit Prefs", u"Close", None):
+        if NSRunAlertPanel(
+            "Preferences Reset",
+            "Your Preferences have been reset.",
+            "Edit Prefs",
+            "Close",
+            None,
+        ):
             self.showPreferencePanel_(self)
 
     def awakeFromNib(self):
         self.current_edits.setDoubleAction_("gotoEdit:")
-        self.sync_message.setStringValue_(u'')
+        self.sync_message.setStringValue_("")
         self.finish.setEnabled_(NO)
 
     def init(self):
@@ -210,34 +236,44 @@ class ZemAppDelegate(NSObject):
             self.sud.setPersistentDomain_forName_(
                 NSDictionary.dictionaryWithContentsOfFile_(
                     NSBundle.mainBundle().pathForResource_ofType_(
-                        self.bundleIdent, "plist")),
-                self.bundleIdent)
+                        self.bundleIdent, "plist"
+                    )
+                ),
+                self.bundleIdent,
+            )
             self.sud.synchronize()
 
-        version_check = self.sud.stringForKey_(u'version_check')
+        version_check = self.sud.stringForKey_("version_check")
         if version_check != __version__:
-            response =  NSRunAlertPanel(u"Preferences Differ",
-                                        u"Your Preferences were set with "
-                                        "an older version of "
-                                        "Zem. Would you "
-                                        "like to upgrade your Preferences?",
-                                        u"Upgrade", u"Start Fresh", None)
+            response = NSRunAlertPanel(
+                "Preferences Differ",
+                "Your Preferences were set with "
+                "an older version of "
+                "Zem. Would you "
+                "like to upgrade your Preferences?",
+                "Upgrade",
+                "Start Fresh",
+                None,
+            )
             if response == 1:
                 self.upgradePrefs()
             if response == 0:
                 self.resetPrefs()
 
-        interval = (self.sud.floatForKey_(u'save_interval')) or 20.0
+        interval = (self.sud.floatForKey_("save_interval")) or 20.0
+
         timer = (
-    NSTimer.scheduledTimerWithTimeInterval_target_selector_userInfo_repeats_(
-                interval, self, 'updateIfModified', self, YES))
-        NSRunLoop.currentRunLoop().addTimer_forMode_(timer,
-                                                     NSDefaultRunLoopMode)
+            NSTimer.scheduledTimerWithTimeInterval_target_selector_userInfo_repeats_(
+                interval, self, "updateIfModified", self, YES
+            )
+        )
+        NSRunLoop.currentRunLoop().addTimer_forMode_(timer, NSDefaultRunLoopMode)
 
         return self
 
     def applicationWillTerminate_(self, notification):
         for x in range(len(self.current_edits_data)):
             del self.current_edits_data[x]
+
 
 AppHelper.runEventLoop(argv=[])
--- setup.py
+++ setup.py
@@ -1,50 +1,54 @@
 from setuptools import setup
 
 plist = dict(
-    CFBundleName = "Zem",
-    CFBundleIdentifier = "com.urbanape.zopeeditmanager",
-    CFBundleShortVersionString = "Version 0.9.9",
-    CFBundleGetInfoString = ("Zem version 0.9.9, "
-                             "Copyright 2003-2006 Zope Foundation"),
-    NSHumanReadableCopyright = "Copyright 2003-2006 Zope Foundation",
-    CFBundleIconFile = "ZEM.icns",
-    CFBundleInfoDictionaryVersion = "6.0",
-    CFBundlePackageType = "APPL",
-    CFBundleSignature = "ZEDM",
-    CFBundleVersion = "0.9.9",
-    NSMainNibFile = "MainMenu",
-    NSPrincipalClass = "NSApplication",
-    CFBundleDevelopmentRegion = "English",
-    CFBundleDocumentTypes = [
-        dict(CFBundleTypeExtensions = ["zem"],
-             CFBundleTypeOSTypes = ["ZEMD"],
-             CFBundleTypeMIMETypes = ["application/x-zope-edit"],
-             CFBundleTypeRole = "Viewer",
-             CFBundleTypeIconFile = "ZEMDocument.icns")])
+    CFBundleName="Zem",
+    CFBundleIdentifier="com.urbanape.zopeeditmanager",
+    CFBundleShortVersionString="Version 0.9.9",
+    CFBundleGetInfoString=("Zem version 0.9.9, Copyright 2003-2006 Zope Foundation"),
+    NSHumanReadableCopyright="Copyright 2003-2006 Zope Foundation",
+    CFBundleIconFile="ZEM.icns",
+    CFBundleInfoDictionaryVersion="6.0",
+    CFBundlePackageType="APPL",
+    CFBundleSignature="ZEDM",
+    CFBundleVersion="0.9.9",
+    NSMainNibFile="MainMenu",
+    NSPrincipalClass="NSApplication",
+    CFBundleDevelopmentRegion="English",
+    CFBundleDocumentTypes=[
+        dict(
+            CFBundleTypeExtensions=["zem"],
+            CFBundleTypeOSTypes=["ZEMD"],
+            CFBundleTypeMIMETypes=["application/x-zope-edit"],
+            CFBundleTypeRole="Viewer",
+            CFBundleTypeIconFile="ZEMDocument.icns",
+        )
+    ],
+)
 
 options = {
-    'iconfile': 'Resources/ZEM.icns',
+    "iconfile": "Resources/ZEM.icns",
 }
 
 setup(
-    data_files = ["Nibs/MainMenu.nib",
-                  "Nibs/Preferences.nib",
-                  "Resources/Files.png",
-                  "Resources/HelperApps.png",
-                  "Resources/WebDAV.png",
-                  "Resources/ZEM.icns",
-                  "Resources/ZEMDocument.icns",
-                  "Resources/add_disabled.png",
-                  "Resources/add_idle.png",
-                  "Resources/add_pressed.png",
-                  "Resources/remove_disabled.png",
-                  "Resources/remove_idle.png",
-                  "Resources/remove_pressed.png",
-                  "Resources/com.urbanape.zopeeditmanager.plist"
-                  ],
-    app = [dict(
-        script="ZemAppDelegate.py", plist=plist),
-           ],
-    options={'py2app': options},
-    setup_requires = 'py2app',
-    )
+    data_files=[
+        "Nibs/MainMenu.nib",
+        "Nibs/Preferences.nib",
+        "Resources/Files.png",
+        "Resources/HelperApps.png",
+        "Resources/WebDAV.png",
+        "Resources/ZEM.icns",
+        "Resources/ZEMDocument.icns",
+        "Resources/add_disabled.png",
+        "Resources/add_idle.png",
+        "Resources/add_pressed.png",
+        "Resources/remove_disabled.png",
+        "Resources/remove_idle.png",
+        "Resources/remove_pressed.png",
+        "Resources/com.urbanape.zopeeditmanager.plist",
+    ],
+    app=[
+        dict(script="ZemAppDelegate.py", plist=plist),
+    ],
+    options={"py2app": options},
+    setup_requires="py2app",
+)
